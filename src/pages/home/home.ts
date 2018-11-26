de7b9d77a56c172052e7d6c5cdf457e139cf7d99
Nick Corl <nick@crucibledevlabs.com>
2018-11-26 11:14:44 -0700

    first and only commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  myArray = ['this', 'is','my','first','ngFor'];
+  newItem = "";
   constructor(public navCtrl: NavController) {
 
   }
+  test(){
+    console.log("test");
+  }
+  removeItem(item){
+    var index = this.myArray.indexOf(item);
+    this.myArray.splice(index,1);
+    console.log(this.myArray);
+  }
+  addItem(){
+    if(this.newItem != ""){
+    this.myArray.push(this.newItem);
+      this.newItem = "";
+    }
+    console.log(this.newItem);
 
+  }
 }
